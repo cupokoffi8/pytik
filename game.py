67f48577280b3b40b963b526682fa2f77e2ff74a
Alex Gaskins <cupokoffi@gmail.com>
2023-04-18 20:57:38 -0400

    Almost not bad

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import random 
 
 # Initialize the board
 board = ['-'] * 9
@@ -16,51 +17,47 @@ current_player = players[0]
 def draw_board():
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.button(board[0], key=0, on_click=lambda: update_board(0))
+        st.button(board[0], key="button"+str(random.random()), on_click=lambda: update_board(0))
     with col2:
-        st.button(board[1], key=1, on_click=lambda: update_board(1))
+        st.button(board[1], key="button"+str(random.random()), on_click=lambda: update_board(1))
     with col3:
-        st.button(board[2], key=2, on_click=lambda: update_board(2))
+        st.button(board[2], key="button"+str(random.random()), on_click=lambda: update_board(2))
     with col1:
-        st.button(board[3], key=3, on_click=lambda: update_board(3))
+        st.button(board[3], key="button"+str(random.random()), on_click=lambda: update_board(3))
     with col2:
-        st.button(board[4], key=4, on_click=lambda: update_board(4))
+        st.button(board[4], key="button"+str(random.random()), on_click=lambda: update_board(4))
     with col3:
-        st.button(board[5], key=5, on_click=lambda: update_board(5))
+        st.button(board[5], key="button"+str(random.random()), on_click=lambda: update_board(5))
     with col1:
-        st.button(board[6], key=6, on_click=lambda: update_board(6))
+        st.button(board[6], key="button"+str(random.random()), on_click=lambda: update_board(6))
     with col2:
-        st.button(board[7], key=7, on_click=lambda: update_board(7))
+        st.button(board[7], key="button"+str(random.random()), on_click=lambda: update_board(7))
     with col3:
-        st.button(board[8], key=8, on_click=lambda: update_board(8))
+        st.button(board[8], key="button"+str(random.random()), on_click=lambda: update_board(8))
+ 
 
 def check_game_over():
     # check for horizontal wins
     for i in range(0, 9, 3):
         if board[i] == board[i+1] == board[i+2] != " ":
             return board[i]
-
     # check for vertical wins
     for i in range(3):
         if board[i] == board[i+3] == board[i+6] != " ":
             return board[i]
-
     # check for diagonal wins
     if board[0] == board[4] == board[8] != " ":
         return board[0]
-
     if board[2] == board[4] == board[6] != " ":
         return board[2]
-
     # check for a tie
     if " " not in board:
         return "TIE"
-
     # if there is no winner and the game is not a tie, return False
     return False 
 
 # Define the function to update the board
-def update_board(button_id):
+def update_board(button_id): 
     global board, current_player
 
     # check if the button has already been clicked
@@ -71,32 +68,39 @@ def update_board(button_id):
     # update the board with the current player's symbol
     board[button_id] = current_player
 
-    # check for a win or a tie
-    game_over = check_game_over()
-
     # switch to the other player
     current_player = "X" if current_player == "O" else "O"
+
+    # check for a win or a tie
+    game_over = check_game_over()
 
     # redraw the board
     draw_board()
 
     # display a message if the game is over
     if game_over:
+        st.write("Current Player: ", current_player)
         if game_over == "TIE":
             st.warning("The game is a tie!")
         else:
             st.success(f"Player {game_over} wins!")
+        # disable all the buttons
+        for i in range(9):
+            st.session_state[f"button_{i}_disabled"] = True
 
 
-# Define the main function
 def main():
-    st.title("Tic Tac Toe")
+    # Initialize the session state variables for the buttons
+    for i in range(9):
+        st.session_state[f"button_{i}_disabled"] = False
 
     # Draw the board
     draw_board()
 
+    st.title("Tic Tac Toe")
     # Display the current player
     st.write("Current Player: ", current_player)
+
 
 if __name__ == "__main__":
     main()
